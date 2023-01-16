e968a9dd3d22aab524c25360a8afd0058fbc7e8f
Conrad Wood <cnw@conradwood.net>
2023-01-16 06:25:56 +0000

    updated to go-easyops version 15252
    Change-Id: I8854f885fbb9abc8c65a3cc0210ff21b2d9e8b16

--- src/golang.conradwood.net/apis/weblogin/go.mod
+++ src/golang.conradwood.net/apis/weblogin/go.mod
@@ -1,3 +1,3 @@
 module golang.conradwood.net/apis/weblogin
 
-go 1.17
+go 1.18
--- src/golang.conradwood.net/weblogin/common/sign.go
+++ src/golang.conradwood.net/weblogin/common/sign.go
@@ -13,7 +13,6 @@ import (
 	os "golang.conradwood.net/apis/objectstore"
 	"golang.conradwood.net/go-easyops/authremote"
 	"golang.conradwood.net/go-easyops/client"
-	"golang.conradwood.net/go-easyops/tokens"
 	"golang.conradwood.net/go-easyops/utils"
 )
 
@@ -34,7 +33,7 @@ func InitKey() {
 		ostore = os.NewObjectStoreClient(client.Connect("objectstore.ObjectStore"))
 	}
 	ctx := authremote.Context()
-	ctx = tokens.ContextWithToken()
+	ctx = authremote.Context()
 	p, err := ostore.TryGet(ctx, &os.GetRequest{ID: KEY_ID})
 	if err != nil {
 		fmt.Printf("Cannot get key: %s\n", utils.ErrorString(err))
