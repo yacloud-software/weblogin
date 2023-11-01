f66f464ff55af208f4c6abacf590298deed50286
Conrad Wood <cnw@conradwood.net>
2023-11-01 16:42:41 +0000

    a bit of docu
    Change-Id: Ia5e64709bf1a5df72fea140913ff25a76243290e

--- src/golang.conradwood.net/weblogin/requests/session.go
+++ src/golang.conradwood.net/weblogin/requests/session.go
@@ -6,6 +6,13 @@ import (
 	"golang.conradwood.net/weblogin/requesttracker"
 )
 
+// this needs to check if there is a session (e.g. as cookie).
+// if not, create a new one
+// if there is, reuse it
+// then, redirect browser back to where it came from (using the same method, payload etc)
+// the last redirect needs to include some short-lived token, which h2gproxy can use to
+// lookup the session
+// (it is h2gproxy's job to then set the session cookie on the domain)
 func needSessionPage(cr *requesttracker.Request) (*pb.WebloginResponse, error) {
 	return nil, fmt.Errorf("need session not implemented")
 }
