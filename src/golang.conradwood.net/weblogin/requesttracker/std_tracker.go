0a9c569d3e46748ccbb3e3a3059d602745fd452b
Conrad Wood <cnw@conradwood.net>
2023-11-02 13:48:44 +0000

    added retryqueue to activitylog so we can cope with small service outages of the logging service
    Change-Id: I80f2726aac2958e130ebf59d12cb8ebfb592256c

--- src/golang.conradwood.net/weblogin/requesttracker/std_tracker.go
+++ src/golang.conradwood.net/weblogin/requesttracker/std_tracker.go
@@ -10,16 +10,20 @@ import (
 	"golang.conradwood.net/go-easyops/utils"
 	"golang.conradwood.net/weblogin/opts"
 	"google.golang.org/grpc"
+	"sync"
 	"time"
 )
 
 var (
 	logger_chan = make(chan *logreq, 100)
 	service_map = make(map[string]*logservice)
+	retries     []*logreq
+	retry_lock  sync.Mutex
 )
 
 func init() {
 	go log_chan()
+	go log_chan_retry()
 }
 
 // TODO: maybe implement a "cleaner" if use many different services
@@ -30,7 +34,10 @@ type logservice struct {
 	last_used   time.Time
 }
 type logreq struct {
-	req *pb.AuthActivityRequest
+	req          *pb.AuthActivityRequest
+	last_attempt time.Time
+	done         bool
+	counter      int
 }
 
 type StandardTracker struct {
@@ -52,11 +59,15 @@ func (st *StandardTracker) LogActivity(action pb.AuthAction, req *pb.AuthActivit
 func log_chan() {
 	for {
 		lr := <-logger_chan
+		lr.last_attempt = time.Now()
 		ls := get_service()
 		ctx := authremote.ContextWithTimeout(time.Duration(500) * time.Millisecond)
 		_, err := ls.LogActivity(ctx, lr.req)
 		if err != nil {
 			fmt.Printf("FAILED TO LOG ACTIVITY (to service \"%s\"): %s\n", ls.servicename, utils.ErrorString(err))
+			retry_lock.Lock()
+			retries = append(retries, lr)
+			retry_lock.Unlock()
 		}
 
 	}
@@ -82,4 +93,41 @@ func (ls *logservice) LogActivity(ctx context.Context, req *pb.AuthActivityReque
 		return nil, err
 	}
 	return out, nil
+}
+
+func log_chan_retry() {
+	for {
+		time.Sleep(time.Duration(3) * time.Second)
+		retry_lock.Lock()
+		rs := retries
+		retry_lock.Unlock()
+		for _, r := range rs {
+			if time.Since(r.last_attempt) < time.Duration(4)*time.Second {
+				continue
+			}
+			r.counter++
+			r.last_attempt = time.Now()
+			ls := get_service()
+			ctx := authremote.ContextWithTimeout(time.Duration(1500) * time.Millisecond)
+			_, err := ls.LogActivity(ctx, r.req)
+			if err == nil {
+				r.done = true
+
+			}
+
+		}
+
+		// now remove all those that are "done" or broken from our retry queue
+		retry_lock.Lock()
+		var rsn []*logreq
+		for _, r := range retries {
+			if r.done || r.counter > 1000 {
+				continue
+			}
+			rsn = append(rsn, r)
+		}
+		retries = rsn
+		retry_lock.Unlock()
+
+	}
 }
