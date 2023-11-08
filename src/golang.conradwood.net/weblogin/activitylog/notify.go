d43f4bab0616da55196912d81940984267f10c1f
Conrad Wood <cnw@conradwood.net>
2023-11-08 14:16:15 +0000

    automatic commit
    Change-Id: I56b39d2cc17ac019bbd685f41c51c00852779cd5

--- src/golang.conradwood.net/weblogin/activitylog/notify.go
+++ src/golang.conradwood.net/weblogin/activitylog/notify.go
@@ -1,15 +1,20 @@
 package activitylog
 
 import (
+	"flag"
 	"fmt"
 	slack "golang.conradwood.net/apis/slackgateway"
 	"golang.conradwood.net/go-easyops/authremote"
 	"golang.conradwood.net/go-easyops/utils"
 )
 
+var (
+	slack_userid = flag.String("slack_userid", "7", "userid for notifications")
+)
+
 func send_notification(format string, args ...interface{}) {
 	text := fmt.Sprintf(format, args...)
-	p := &slack.PostRequest{UserID: "7", Text: text}
+	p := &slack.PostRequest{UserID: *slack_userid, Text: text}
 	go func(pr *slack.PostRequest) {
 		ctx := authremote.Context()
 		_, err := slack.GetSlackGatewayClient().Post(ctx, pr)
